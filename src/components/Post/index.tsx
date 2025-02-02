f204353b3d8c513662c14e003017d894924aa38e
mitukun32 <mitukun32@gmail.com>
2025-02-02 13:24:22 +0900

    ファイル一式

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,17 +1,24 @@
 // rafceと入力後タブキーをおす🤗
 
-import React from 'react'
-import styles from './style.module.scss'
+import React from "react";
+import styles from "./style.module.scss";
 
-const Post = () => {
+type PostProps = {
+  id: number;
+  name: string;
+  date: string;
+  content: string;
+};
+
+const Post: React.FC<PostProps> = ({ name, date, content }) => {
   return (
     <div className={styles.post}>
-      <p>名前が入る予定</p>
-      <p>日付が入る予定</p>
-      <p>登録されたデータを表示する予定</p>
+      <p>{name}</p>
+      <p>{date}</p>
+      <p>{content}</p>
       <p className={styles.link}>リンクが入ります</p>
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,59 @@ import Header from "@/components/Header";
 import styles from "../styles/style.module.scss";
 import Timeline from "@/components/Timeline";
 import Post from "@/components/Post";
+import Button from "@/components/Button";
+import { useEffect, useState } from "react";
+
+// MEMO: データベースから取得しているイメージです🤗（API）
+const dummyData: any = [
+  {
+    id: 1,
+    name: "山田 太郎",
+    date: "2024-02-01",
+    content: "今日はReactの勉強をしました！",
+  },
+  {
+    id: 2,
+    name: "佐藤 花子",
+    date: "2024-02-02",
+    content: "新しいプロジェクトが始まりました。",
+  },
+  {
+    id: 3,
+    name: "鈴木 一郎",
+    date: "2024-02-03",
+    content: "Next.jsのSSRを試しています。",
+  },
+  {
+    id: 4,
+    name: "田中 桃子",
+    date: "2024-02-04",
+    content: "UIデザインの改善を行いました。",
+  },
+  {
+    id: 5,
+    name: "中村 健",
+    date: "2024-02-05",
+    content: "TypeScriptの型安全性について学習中。",
+  },
+];
 
 export default function Home() {
+  // useStateを貼り付けてください🤗
+  const [posts, setPosts] = useState([]);
+
+  // useEffectの処理=画面が表示された直後に１度実行されます🤗
+  useEffect(() => {
+    // ここに書く
+
+    setPosts(dummyData);
+    console.log(11111111);
+    // ここの下消さない
+  }, []);
+
+  // 追加
+  console.log(posts, "データ");
+
   return (
     <>
       <Head>
@@ -20,7 +71,19 @@ export default function Home() {
 
         <Timeline />
 
-        <Post />
+        {/* 後で消しますが先に表示の確認をしましょう🤗 */}
+        {/* <Button textColor="pink" bg="blue" /> */}
+
+        {/* 表示する時のテクニック🤗 */}
+        {posts.map((item, index) => (
+          <Post
+            key={index}
+            id={item.id}
+            name={item.name}
+            date={item.date}
+            content={item.content}
+          />
+        ))}
         {/*  */}
       </main>
     </>
